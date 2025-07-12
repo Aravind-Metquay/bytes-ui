ff29b6915e526180a6c4032bfb4406886ff3d835
Aravind-Metquay <119470767+Aravind-Metquay@users.noreply.github.com>
2025-07-12 10:22:51 +0530

    Add bits-ui dependency and implement Button component
    - Updated package.json to include bits-ui as a dependency.
    - Modified +page.svelte to use the Button component from bits-ui.
    - Created button.svelte component for rendering a styled button.
    - Updated components index to export the new Button component.

---
+++ src/lib/components/button/button.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import { Button } from 'bits-ui';
+</script>
+
+	<Button.Root
+		class="rounded-input bg-gray-900 text-gray-50 shadow-mini hover:bg-gray-900/95 inline-flex
+		h-12 items-center justify-center px-[21px] text-[15px]
+		font-semibold active:scale-[0.98] active:transition-all border rounded-md">
+		This is a button
+	</Button.Root>
